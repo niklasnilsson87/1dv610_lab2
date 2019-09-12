b79291ac0aa375c64aa7338371c7eb4b9cf8b3fa
Niklas Nilsson <nn222ia@student.lnu.se>
2019-09-12 13:34:02 +0200

    working on date

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,14 @@ class DateTimeView {
 
 
 	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+		$day = date('l');
+		$date = date('d');
+		$month = date('F');
+		$year = date('Y');
+		ini_set('date.timezone', 'Europe/Stockholm');
+		$time = date('H:i:s', time() - date('Z'));
+		$timeString = '';
+		$timeString .= "$day, the $date" . "th of $month $year, The time is $time";
 
 		return '<p>' . $timeString . '</p>';
 	}
