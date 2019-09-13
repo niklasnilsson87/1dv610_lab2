60efe78cc98f0178d28d28095bd100666a1af121
Niklas Nilsson <nn222ia@student.lnu.se>
2019-09-13 11:22:16 +0200

    testing on heroku

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
 require_once('model/Database.php');
 require_once('model/LoginModel.php');
-require_once('controller/LoginController.php');
+// require_once('controller/LoginController.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
@@ -18,7 +18,7 @@ $dtv = new DateTimeView();
 $lv = new LayoutView();
 $db = new Database();
 $loginModel = new LoginModel($db);
-$loginController = new LoginController($v, $loginModel);
+// $loginController = new LoginController($v, $loginModel);
 
 $user = $db->getUser();
 echo $user[0]["username"];
